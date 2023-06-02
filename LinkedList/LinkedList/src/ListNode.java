2fccfd0e513b10d84ae642af1d2fa73258ad5ae5
Kamal <kamalabdullayevv@gmail.com>
2023-06-02 13:50:04 +0400

    feat: Linked List

---
+++ LinkedList/LinkedList/src/ListNode.java
@@ -0,0 +1,8 @@
+public class ListNode<T> {
+    int val;
+    ListNode<T> next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+}

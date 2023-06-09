09f33c0a453eb9dcfac4215ee05c0827b2984fdd
Kamal <kamalabdullayevv@gmail.com>
2023-06-09 20:32:10 +0400

    feat: Queue & Stack

---
+++ Stack/src/BaseBall.java
@@ -0,0 +1,8 @@
+public class BaseBall {
+
+    // 682. Baseball Game
+
+    public static int calPoints(String[] operations) {
+        Stack<String> scoreList = 
+    }
+}
---
+++ Stack/src/Theory/Stack.java
@@ -0,0 +1,2 @@
+package Theory;public class Stack {
+}

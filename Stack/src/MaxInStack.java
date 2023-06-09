a91384f15d41380b264c7bec87a63129eab1975b
Kamal <kamalabdullayevv@gmail.com>
2023-06-09 20:32:18 +0400

    feat: Queue & Stack

--- Stack/src/MaxInStack.java
+++ Stack/src/MaxInStack.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class MaxInStack {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class MaxInStack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(20);
+        stack.push(15);
+        stack.push(90);
+        stack.push(40);
+        stack.push(70);
+
+        System.out.println(maxInStack(stack));
+    }
+
+    public static int maxInStack(Stack<Integer> stack) {
+        int temp = 0;
+
+        while (!stack.empty()) {
+            int num = stack.pop();
+            temp = num > temp ? num : temp;
+        }
+
+        return temp;
+    }
 }
--- Stack/src/Theory/Node.java
+++ Stack/src/Theory/Node.java
@@ -1,2 +1,32 @@
-package Theory;public class Node {
+package Theory;
+
+public class Node<T> {
+
+    T data;
+    Node<T> nextNode;
+
+    @Override
+    public String toString() {
+        return "" + data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public Node(T data) {
+        this.data = data;
+    }
 }
